ed76ad3164c90be81e33f7cc974bb8ea21252ee7
Lori.wang <wangzhengguang@starlinke.com>
2021-03-11 10:38:51 +0800

    初步完成主仓库与子仓库的嵌套

--- .eslintrc.js
+++ .eslintrc.js
@@ -191,7 +191,7 @@ module.exports = {
     'yield-star-spacing': [2, 'both'],
     'yoda': [2, 'never'],
     'prefer-const': 2,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': 0,
     'object-curly-spacing': 'off',
     'array-bracket-spacing': [2, 'never']
   }
